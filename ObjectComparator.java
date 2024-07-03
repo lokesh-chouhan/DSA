f0b6c82d5222238b42713d90747bcf33a532a2ec
lokesh chouhan <lokesh.han87@gmail.com>
2024-07-03 15:06:43 +0530

    added comparator and priority queue

--- ObjectComparator.java
+++ ObjectComparator.java
@@ -1,4 +1,98 @@
 package com.dsa.randomquesgenerator;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
 public class ObjectComparator {
+
+    public static void main(String[] args) {
+
+        ArrayList<Pair> list = new ArrayList<>();
+
+        int ar[][] = {{2,9}, {1,7}, {3,4}};
+
+        for(int i=0;i< ar.length;i++){
+
+            list.add(new Pair(ar[i][0], ar[i][1]));
+        }
+
+        for(Pair p:list){
+            System.out.println(p.a+"   "+p.b);
+        }
+
+        System.out.println("==========================");
+        Collections.sort(list, new IncreaseOnA());
+
+        for(Pair p:list){
+            System.out.println(p.a+"   "+p.b);
+        }
+
+        System.out.println("==========================");
+        Collections.sort(list, new DecreaseOnA());
+
+        for(Pair p:list){
+            System.out.println(p.a+"   "+p.b);
+        }
+
+        System.out.println("==========================");
+        Collections.sort(list, new IncreaseOnB());
+
+        for(Pair p:list){
+            System.out.println(p.a+"   "+p.b);
+        }
+
+        System.out.println("==========================");
+        Collections.sort(list, new DecreaseOnB());
+
+        for(Pair p:list){
+            System.out.println(p.a+"   "+p.b);
+        }
+
+    }
+
+}
+
+class Pair{
+
+    int a;
+    int b;
+    Pair(int a, int b){
+        this.a = a;
+        this.b= b;
+    }
+}
+
+class IncreaseOnA implements Comparator<Pair>{
+
+    @Override
+    public int compare(Pair p1, Pair p2) {
+        return p1.a - p2.a;
+        //return 0;
+    }
+}
+
+class DecreaseOnA implements Comparator<Pair>{
+
+    @Override
+    public int compare(Pair p1, Pair p2) {
+        return p2.a - p1.a;
+    }
+}
+
+class IncreaseOnB implements Comparator<Pair>{
+
+
+    @Override
+    public int compare(Pair p1, Pair p2) {
+        return p1.b - p2.b;
+    }
+}
+
+class DecreaseOnB implements Comparator<Pair>{
+
+    @Override
+    public int compare(Pair p1, Pair p2) {
+        return p2.b - p1.b;
+    }
 }
--- PQDemo.java
+++ PQDemo.java
@@ -1,4 +1,94 @@
 package com.dsa.randomquesgenerator;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 public class PQDemo {
+
+    public static void main(String args[]){
+
+        int ar[][] = {{2,9},{1,7},{3,4}};
+
+        PriorityQueue<Pair1> pq1 = new PriorityQueue<>(new IncreaseOnA1());
+        PriorityQueue<Pair1> pq2 = new PriorityQueue<>(new DecreaseOnA1());
+        PriorityQueue<Pair1> pq3 = new PriorityQueue<>(new IncreaseOnB1());
+        PriorityQueue<Pair1> pq4 = new PriorityQueue<>(new DecreaseOnB1());
+        for(int i=0;i<ar.length;i++){
+            pq1.add(new Pair1(ar[i][0], ar[i][1]));
+            pq2.add(new Pair1(ar[i][0], ar[i][1]));
+            pq3.add(new Pair1(ar[i][0], ar[i][1]));
+            pq4.add(new Pair1(ar[i][0], ar[i][1]));
+            System.out.println(ar[i][0]+"  "+ar[i][1]);
+        }
+
+        System.out.println("============================");
+
+        while(!pq1.isEmpty()){
+            Pair1 p = pq1.remove();
+            System.out.println(p.a+"  "+p.b);
+        }
+
+        System.out.println("============================");
+
+        while(!pq2.isEmpty()){
+            Pair1 p = pq2.remove();
+            System.out.println(p.a+"  "+p.b);
+        }
+
+        System.out.println("============================");
+
+        while(!pq3.isEmpty()){
+            Pair1 p = pq3.remove();
+            System.out.println(p.a+"  "+p.b);
+        }
+
+        System.out.println("============================");
+
+        while(!pq4.isEmpty()){
+            Pair1 p = pq4.remove();
+            System.out.println(p.a+"  "+p.b);
+        }
+
+
+
+    }
+}
+
+class Pair1{
+
+    int a;
+    int b;
+    Pair1(int a, int b){
+
+        this.a = a;
+        this.b = b;
+    }
+}
+
+class IncreaseOnA1 implements Comparator<Pair1>{
+    @Override
+    public int compare(Pair1 p1, Pair1 p2) {
+        return p1.a - p2.a;
+    }
+}
+
+class DecreaseOnA1 implements Comparator<Pair1>{
+    @Override
+    public int compare(Pair1 p1, Pair1 p2) {
+        return p2.a - p1.a;
+    }
+}
+
+class IncreaseOnB1 implements Comparator<Pair1>{
+    @Override
+    public int compare(Pair1 p1, Pair1 p2) {
+        return p1.b - p2.b;
+    }
+}
+
+class DecreaseOnB1 implements Comparator<Pair1>{
+    @Override
+    public int compare(Pair1 p1, Pair1 p2) {
+        return p2.b - p1.b;
+    }
 }
